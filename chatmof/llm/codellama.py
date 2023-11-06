71a211003c1b8fadd525f481afafa72e47a90abd
yeonghun kang <dudgns1675@kaist.ac.kr>
2023-11-06 17:00:58 +0900

    231106

---
+++ chatmof/llm/codellama.py
@@ -0,0 +1,43 @@
+import torch
+from transformers import LlamaForCausalLM, CodeLlamaTokenizer, pipeline
+from langchain import HuggingFacePipeline
+from langchain.base_language import BaseLanguageModel
+
+
+CodeLLAMA_HF = [
+    'CodeLlama-7b-Python-hf',
+    'CodeLlama-13b-Python-hf',
+    'CodeLlama-34b-Python-hf',
+]
+
+
+def get_codellama_llm(model_name: str, max_token: int =4096, temperature: float=0.1) -> BaseLanguageModel:
+    if model_name not in CodeLLAMA_HF:
+        raise ValueError(f'model_name should be one of {CodeLLAMA_HF}, not {model_name}')
+    model_name = f'codellama/{model_name}'
+
+    tokenizer = CodeLlamaTokenizer.from_pretrained(model_name,
+                                            use_auth_token=True,
+                                            )
+    model = LlamaForCausalLM.from_pretrained(model_name,
+                                                device_map='auto',
+                                                torch_dtype=torch.float16,
+                                                use_auth_token=True,
+                                                )
+
+    pipe = pipeline("text-generation",
+                model=model,
+                tokenizer=tokenizer,
+                torch_dtype=torch.bfloat16,
+                device_map="auto",
+                do_sample=True,
+                top_k=30,
+                max_new_tokens=max_token,
+                eos_token_id=tokenizer.eos_token_id
+                )
+
+    llm = HuggingFacePipeline(pipeline = pipe, model_kwargs = {'temperature':temperature})
+    return llm
+
+
+
--- chatmof/llm/utils.py
+++ chatmof/llm/utils.py
@@ -1,15 +1,18 @@
 from chatmof.llm.llama import get_llama_llm
 from chatmof.llm.openai import get_openai_llm
+from chatmof.llm.codellama import get_codellama_llm
 from langchain.base_language import BaseLanguageModel
 
 
 def get_llm(model_name: str, temperature: float = 0.1, max_tokens: int = 4096) -> BaseLanguageModel:
-    model_name = model_name.lower()
+    m_name = model_name.lower()
 
-    if model_name.startswith('gpt'):
+    if m_name.startswith('gpt'):
         llm = get_openai_llm(model_name, temperature=temperature)
-    elif model_name.startswith('llama'):
+    elif m_name.startswith('llama'):
         llm = get_llama_llm(model_name, max_token=max_tokens, temperature=temperature)
+    elif m_name.startswith('codellama'):
+        llm = get_codellama_llm(model_name, max_token=max_tokens, temperature=temperature)
     else:
         raise ValueError(f'model_name should be one of llama and gpt, not {model_name}')
     
